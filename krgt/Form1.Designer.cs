bef523df250a36460a78462a135a06767aa8b6d4
km <km@bk.ru>
2015-12-16 21:25:11 +0200

    1 test and form

---
+++ krgt/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace krgt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnshow = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbmenu = new System.Windows.Forms.ListBox();
+            this.lbshowworkers = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbworker = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbdesertbehavior = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbdrinkbehavior = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbmainsauce = new System.Windows.Forms.ListBox();
+            this.btnorder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lborder = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbdrink = new System.Windows.Forms.ListBox();
+            this.labeldesert = new System.Windows.Forms.Label();
+            this.lbdesert = new System.Windows.Forms.ListBox();
+            this.labelsalat = new System.Windows.Forms.Label();
+            this.lbsalat = new System.Windows.Forms.ListBox();
+            this.labelpotato = new System.Windows.Forms.Label();
+            this.lbpotato = new System.Windows.Forms.ListBox();
+            this.labelsandwich = new System.Windows.Forms.Label();
+            this.lbsandwich = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(533, 147);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(88, 23);
+            this.btnshow.TabIndex = 53;
+            this.btnshow.Text = "show";
+            this.btnshow.UseVisualStyleBackColor = true;
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(855, 20);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(21, 23);
+            this.btndelete.TabIndex = 52;
+            this.btndelete.Text = "-";
+            this.btndelete.UseVisualStyleBackColor = true;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(828, 20);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(21, 23);
+            this.btnadd.TabIndex = 51;
+            this.btnadd.Text = "+";
+            this.btnadd.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(546, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Econom menu";
+            // 
+            // lbmenu
+            // 
+            this.lbmenu.FormattingEnabled = true;
+            this.lbmenu.Location = new System.Drawing.Point(536, 324);
+            this.lbmenu.Name = "lbmenu";
+            this.lbmenu.Size = new System.Drawing.Size(131, 121);
+            this.lbmenu.TabIndex = 49;
+            // 
+            // lbshowworkers
+            // 
+            this.lbshowworkers.FormattingEnabled = true;
+            this.lbshowworkers.Location = new System.Drawing.Point(533, 173);
+            this.lbshowworkers.Name = "lbshowworkers";
+            this.lbshowworkers.Size = new System.Drawing.Size(179, 108);
+            this.lbshowworkers.TabIndex = 48;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(533, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Who work ?";
+            // 
+            // lbworker
+            // 
+            this.lbworker.FormattingEnabled = true;
+            this.lbworker.Location = new System.Drawing.Point(533, 46);
+            this.lbworker.Name = "lbworker";
+            this.lbworker.Size = new System.Drawing.Size(120, 95);
+            this.lbworker.TabIndex = 46;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(205, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Desert behavior";
+            // 
+            // lbdesertbehavior
+            // 
+            this.lbdesertbehavior.FormattingEnabled = true;
+            this.lbdesertbehavior.Location = new System.Drawing.Point(208, 186);
+            this.lbdesertbehavior.Name = "lbdesertbehavior";
+            this.lbdesertbehavior.Size = new System.Drawing.Size(96, 95);
+            this.lbdesertbehavior.TabIndex = 44;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "Drink behavior";
+            // 
+            // lbdrinkbehavior
+            // 
+            this.lbdrinkbehavior.FormattingEnabled = true;
+            this.lbdrinkbehavior.Location = new System.Drawing.Point(208, 340);
+            this.lbdrinkbehavior.Name = "lbdrinkbehavior";
+            this.lbdrinkbehavior.Size = new System.Drawing.Size(93, 95);
+            this.lbdrinkbehavior.TabIndex = 42;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Dish with sause";
+            // 
+            // lbmainsauce
+            // 
+            this.lbmainsauce.FormattingEnabled = true;
+            this.lbmainsauce.Location = new System.Drawing.Point(205, 46);
+            this.lbmainsauce.Name = "lbmainsauce";
+            this.lbmainsauce.Size = new System.Drawing.Size(96, 95);
+            this.lbmainsauce.TabIndex = 40;
+            // 
+            // btnorder
+            // 
+            this.btnorder.Location = new System.Drawing.Point(782, 176);
+            this.btnorder.Name = "btnorder";
+            this.btnorder.Size = new System.Drawing.Size(75, 23);
+            this.btnorder.TabIndex = 39;
+            this.btnorder.Text = "OK";
+            this.btnorder.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(766, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Your order";
+            // 
+            // lborder
+            // 
+            this.lborder.FormattingEnabled = true;
+            this.lborder.Location = new System.Drawing.Point(760, 49);
+            this.lborder.Name = "lborder";
+            this.lborder.Size = new System.Drawing.Size(120, 121);
+            this.lborder.TabIndex = 37;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Drink";
+            // 
+            // lbdrink
+            // 
+            this.lbdrink.FormattingEnabled = true;
+            this.lbdrink.Location = new System.Drawing.Point(358, 324);
+            this.lbdrink.Name = "lbdrink";
+            this.lbdrink.Size = new System.Drawing.Size(120, 121);
+            this.lbdrink.TabIndex = 35;
+            // 
+            // labeldesert
+            // 
+            this.labeldesert.AutoSize = true;
+            this.labeldesert.Location = new System.Drawing.Point(355, 160);
+            this.labeldesert.Name = "labeldesert";
+            this.labeldesert.Size = new System.Drawing.Size(38, 13);
+            this.labeldesert.TabIndex = 34;
+            this.labeldesert.Text = "Desert";
+            // 
+            // lbdesert
+            // 
+            this.lbdesert.FormattingEnabled = true;
+            this.lbdesert.Location = new System.Drawing.Point(358, 186);
+            this.lbdesert.Name = "lbdesert";
+            this.lbdesert.Size = new System.Drawing.Size(120, 82);
+            this.lbdesert.TabIndex = 33;
+            // 
+            // labelsalat
+            // 
+            this.labelsalat.AutoSize = true;
+            this.labelsalat.Location = new System.Drawing.Point(355, 24);
+            this.labelsalat.Name = "labelsalat";
+            this.labelsalat.Size = new System.Drawing.Size(31, 13);
+            this.labelsalat.TabIndex = 32;
+            this.labelsalat.Text = "Salat";
+            // 
+            // lbsalat
+            // 
+            this.lbsalat.FormattingEnabled = true;
+            this.lbsalat.Location = new System.Drawing.Point(358, 46);
+            this.lbsalat.Name = "lbsalat";
+            this.lbsalat.Size = new System.Drawing.Size(120, 95);
+            this.lbsalat.TabIndex = 31;
+            // 
+            // labelpotato
+            // 
+            this.labelpotato.AutoSize = true;
+            this.labelpotato.Location = new System.Drawing.Point(77, 340);
+            this.labelpotato.Name = "labelpotato";
+            this.labelpotato.Size = new System.Drawing.Size(38, 13);
+            this.labelpotato.TabIndex = 30;
+            this.labelpotato.Text = "Potato";
+            // 
+            // lbpotato
+            // 
+            this.lbpotato.FormattingEnabled = true;
+            this.lbpotato.Location = new System.Drawing.Point(76, 359);
+            this.lbpotato.Name = "lbpotato";
+            this.lbpotato.Size = new System.Drawing.Size(111, 95);
+            this.lbpotato.TabIndex = 29;
+            // 
+            // labelsandwich
+            // 
+            this.labelsandwich.AutoSize = true;
+            this.labelsandwich.Location = new System.Drawing.Point(77, 24);
+            this.labelsandwich.Name = "labelsandwich";
+            this.labelsandwich.Size = new System.Drawing.Size(54, 13);
+            this.labelsandwich.TabIndex = 28;
+            this.labelsandwich.Text = "Sandwich";
+            // 
+            // lbsandwich
+            // 
+            this.lbsandwich.FormattingEnabled = true;
+            this.lbsandwich.Location = new System.Drawing.Point(77, 46);
+            this.lbsandwich.Name = "lbsandwich";
+            this.lbsandwich.Size = new System.Drawing.Size(110, 277);
+            this.lbsandwich.TabIndex = 27;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 476);
+            this.Controls.Add(this.btnshow);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbmenu);
+            this.Controls.Add(this.lbshowworkers);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbworker);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbdesertbehavior);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbdrinkbehavior);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbmainsauce);
+            this.Controls.Add(this.btnorder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lborder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbdrink);
+            this.Controls.Add(this.labeldesert);
+            this.Controls.Add(this.lbdesert);
+            this.Controls.Add(this.labelsalat);
+            this.Controls.Add(this.lbsalat);
+            this.Controls.Add(this.labelpotato);
+            this.Controls.Add(this.lbpotato);
+            this.Controls.Add(this.labelsandwich);
+            this.Controls.Add(this.lbsandwich);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox lbmenu;
+        private System.Windows.Forms.ListBox lbshowworkers;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox lbworker;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbdesertbehavior;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lbdrinkbehavior;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbmainsauce;
+        private System.Windows.Forms.Button btnorder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lborder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbdrink;
+        private System.Windows.Forms.Label labeldesert;
+        private System.Windows.Forms.ListBox lbdesert;
+        private System.Windows.Forms.Label labelsalat;
+        private System.Windows.Forms.ListBox lbsalat;
+        private System.Windows.Forms.Label labelpotato;
+        private System.Windows.Forms.ListBox lbpotato;
+        private System.Windows.Forms.Label labelsandwich;
+        private System.Windows.Forms.ListBox lbsandwich;
+
+    }
+}
+
---
+++ krgt/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace krgt
+{
+    public partial class Form1 : Form
+    {
+
+                 
+   
+        
+  
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            lborder.SelectionMode = SelectionMode.MultiExtended;
+            lbsandwich.SelectionMode = SelectionMode.MultiExtended;
+            lbpotato.SelectionMode = SelectionMode.MultiExtended;
+            lbsalat.SelectionMode = SelectionMode.MultiExtended;
+            lbdesert.SelectionMode = SelectionMode.MultiExtended;
+            lbdrink.SelectionMode = SelectionMode.MultiExtended;
+            lbdrinkbehavior.SelectionMode = SelectionMode.MultiExtended;
+            lbdesertbehavior.SelectionMode = SelectionMode.MultiExtended;
+            lbmainsauce.SelectionMode = SelectionMode.MultiExtended;
+            lbmenu.SelectionMode = SelectionMode.MultiExtended;
+
+           
+
+            xmlread(lbdesert, "", "prop1"); //ice cream
+            xmlread(lbdesert, "", "prop2"); //cake
+            xmlread(lbdesert, "", "prop3"); //pie
+
+            xmlread(lbdesertbehavior, "", "prop4"); //caramel
+            xmlread(lbdesertbehavior, "", "prop5"); // chocolate
+            xmlread(lbdesertbehavior, "", "prop6"); // cakesauce
+
+            xmlread(lbpotato, "", "prop7"); //fried
+
+            xmlread(lbmainsauce, "", "prop8"); //potato sauce
+            
+            xmlread(lbsandwich, "", "chick"); //chicken
+            xmlread(lbsandwich, "", "cheese"); //cheeseburger
+            xmlread(lbsandwich, "", "beef"); // beef
+            xmlread(lbsandwich, "", "fish"); //fish
+            xmlread(lbsandwich, "", "gamb"); //gamburger
+
+            xmlread(lbsalat, "", "chickensalat"); // chicken salat
+            xmlread(lbsalat, "", "beefsalat"); //beef salat
+            xmlread(lbsalat, "", "fishsalat"); // fish salat
+
+            xmlread(lbmainsauce, "", "chickensalatsauce"); // chicken salat sauce
+            xmlread(lbmainsauce, "", "beefsalatsauce"); //beef salat sauce
+            xmlread(lbmainsauce, "", "fishsalatsauce"); // fish salat sauce
+
+            xmlread(lbdrink, "", "sodawater"); //soda water
+            xmlread(lbdrink, "", "water"); //mineral water
+            xmlread(lbdrink, "", "tea"); //tea
+            xmlread(lbdrink, "", "coffee"); //coffee
+
+            xmlread(lbdrinkbehavior, "", "Honey"); //honey
+            xmlread(lbdrinkbehavior, "", "Lemon"); //lemon
+            xmlread(lbdrinkbehavior, "", "Syrop"); //syrop
+        }
+
+        
+
+        private void xmlread(ListBox lb, string getnameditem, string childnote)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem(getnameditem);
+                    if (attr != null)
+                        lb.Items.Add(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - prop
+                    if (childnode.Name == childnote)
+                    {
+                        lb.Items.Add(childnode.InnerText);
+                    }
+                }
+            }
+
+        }
+            private void addlborder(ListBox lb,string s)
+            {
+                          
+                if (lb.Items.Count > 0)
+                    if (lb.SelectedItems.Count != 0)
+                    {
+                        if (lb.SelectedItem.Equals(s))
+                        {
+                            lborder.Items.Add(s);
+                        }
+                    }
+
+            }
+
+        
+
+       
+
+       
+    }
+}
+
+   
+
