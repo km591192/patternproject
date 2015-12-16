0aaf83879b7e88a2e2007df75eb680bc548d9d68
km <km@bk.ru>
2015-12-16 23:04:59 +0200

    test correctly work

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -29,6 +29,34 @@ namespace TestProject1
             Assert.AreEqual(total, 2);
             Assert.IsNotNull(total);
         }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Beverage chicksal = new ChickenSalatwithsauce(new ChickenSalat());
+            double total = 0;
+            total = chicksal.cost();
+            Assert.AreEqual(total, 6.5);
+            Assert.IsNotNull(total);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Beverage beefsal = new BeefSalatwithsauce(new BeefSalat());
+            double total = 0;
+            total = beefsal.cost();
+            Assert.AreEqual(total, 6);
+            Assert.IsNotNull(total);
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Beverage fishsal = new FishSalatwithsauce(new FishSalat());
+            double total = 0;
+            total = fishsal.cost();
+            Assert.AreEqual(total, 5);
+            Assert.IsNotNull(total);
+        }
 
      
 
