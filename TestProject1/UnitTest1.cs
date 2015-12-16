b96e751cbdbc0cb9f778d474914a2ffaa8eae37f
km <km@bk.ru>
2015-12-16 21:26:22 +0200

    1 test

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using krgt;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Beverage tea = new Tea();
+            double total = 0;
+            total = tea.cost();
+            Assert.AreEqual(total, 2);
+            Assert.IsNotNull(total);
+        }
+    }
+}
